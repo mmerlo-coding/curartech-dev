203bf9416f1296dbf68b6e0f6dfa6e74c16458ef
mmerlo-coding <miguel@architekhealth.com>
2025-05-07 01:33:35 -0500

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,11 +9,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 1,
     },
-    {
-      url: "https://www.curartech.com/#contact",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 2,
-    },
   ];
 }
